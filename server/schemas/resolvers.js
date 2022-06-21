ff172535dbb565e626acaf961d23fa10d21fb6cf
emilyk <emily.g.kruse@gmail.com>
2022-06-21 16:11:22 -0400

    thought and user typeDefs and resolvers

--- server/schemas/resolvers.js
+++ server/schemas/resolvers.js
@@ -2,9 +2,28 @@ const { User, Thought } = require('../models');
 
 const resolvers = {
   Query: {
+    // find all thoughts, find all thoughts by username
     thoughts: async (parent, { username }) => {
       const params = username ? { username } : {};
       return Thought.find(params).sort({ createdAt: -1 });
+    },
+    // find one thought by id
+    thought: async (parent, { _id }) => {
+      return Thought.findOne({ _id });
+    },
+    // find all users
+    users: async () => {
+      return User.find()
+        .select('-__v -password')
+        .populate('friends')
+        .populate('thoughts');
+    },
+    // find one user by username
+    user: async (parent, { username }) => {
+      return User.findOne({ username })
+        .select('-__v -password')
+        .populate('friends')
+        .populate('thoughts');
     }
   }
 };
